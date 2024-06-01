07df04eeb3b8b0f968ca60d1cfa8c76cccb7de73
Dániel Zoltán Tolnai <danielz.tolnai@gmail.com>
2024-06-01 16:11:54 +0200

    Send data only when device is not suspended

--- src/main.cpp
+++ src/main.cpp
@@ -16,10 +16,12 @@ void setup()
 
 void loop()
 {
-    if (bh1750.measurementReady()) {
-        auto lux = bh1750.readLightLevel();
-        ambient_light_sensor.update(static_cast<uint32_t>(lux * 10));
-    }
+    if (!USBDevice.isSuspended()) {
+        if (bh1750.measurementReady()) {
+            auto lux = bh1750.readLightLevel();
+            ambient_light_sensor.update(static_cast<uint32_t>(lux * 10));
+        }
 
-    ambient_light_sensor.handle();
+        ambient_light_sensor.handle();
+    }
 }
